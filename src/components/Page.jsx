00cf72c329d8617d9368f364325e6ebb5da21186
starydv7 <pawanstar72926@gmail.com>
2022-08-26 15:51:37 +0530

    todo

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -8,15 +8,38 @@ const Page = () => {
       </div>
       <div className="kg">
         <div>
-          <p className="para1">0 kg</p>
+          <div className="arrow">
+            <div>
+              <img
+                className="image"
+                src="https://d3hsih69yn4d89.cloudfront.net/web/B4D18197600DB9D0F75D7D720B6B9C66.cache.png"
+                alt="arrow"
+              />
+            </div>
+            <div>
+              {" "}
+              <p className="para1">0 kg</p>
+            </div>
+          </div>
           <p className="para2">so far</p>
         </div>
         <div>
-          <p className='para3'>
+          <p className="para3">
             You'll reach your goal of losing<br></br>13 kg on Dec 23.2022
           </p>
         </div>
-      </div>
+          </div>
+          <div className='recent'>
+              <p>
+                  Recent Meal
+              </p>
+              <div className='meal'>
+                  <p className='name-meal'>No food or exercise recorded for this week</p>
+              </div>
+          </div>
+          
+        
+          
     </div>
   );
 }
